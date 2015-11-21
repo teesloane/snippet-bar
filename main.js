e36e9dbd18e23715c7d26b3fa22bd732922abbb5
Tees <tees@Teess-MacBook-Pro.local>
2015-11-21 14:18:55 -0500

    connected menubar

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
+var menubar = require('menubar');
+var mb = menubar();
+
 
 // Report crashes to our server.
 require('crash-reporter').start();
@@ -20,20 +23,8 @@ app.on('window-all-closed', function() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-
-  // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+  mb.on('ready', function ready () {
+    console.log('app is ready')
+    // your app code here
+  })
 });
