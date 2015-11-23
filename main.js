e092d59463c2bbed05ba212d8d73586ae35922a1
Bronek Szulc <bronek@fastmail.fm>
2015-11-23 18:47:39 -0500

    you shall not resize

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 var ipcMain = require('electron').ipcMain;
 var menubar = require('menubar');
 
-var config = {
+var mb = menubar({
   dir: './app',
   icon: './app/static/img/brackets.png',
   width: 600,
   height: 370,
   'always-on-top': true
-};
+});
 
-var mb = menubar(config);
+// prevent window resizing
+mb.on('after-create-window', function() {
+  mb.window.setResizable(false);
+});
 
 // Quit when all windows are closed.
 mb.app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform != 'darwin') {
     mb.app.quit();
   }
@@ -23,15 +24,10 @@ mb.app.on('window-all-closed', function() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 mb.app.on('ready', function() {
-
   ipcMain.on('mb-app', function(event, arg) {
     if (arg === "quit") {
       console.log('goodbye!');
       mb.app.quit();
-    } else if (arg === "config") {
-      // send config
     }
-
-    // event.sender.send('asynchronous-reply', 'pong');
   });
 });
