d376b22d00bf9dde3f9b3159cdf90bbdf6bfa113
broneks <bronek@fastmail.fm>
2015-12-13 13:12:42 -0500

    added tags component -- removed some global styles and flexbox from add-mode which has regressed some of the app styling

--- src/components/AddMode.js
+++ src/components/AddMode.js
@@ -1,5 +1,6 @@
 const React = require('react');
 
+const Tags          = require('./Tags');
 const PanelControls = require('./PanelControls');
 
 const AddMode = React.createClass({
@@ -7,13 +8,8 @@ const AddMode = React.createClass({
     event.preventDefault();
 
     let title = this.refs.title.value.trim();
-
-    let tags = this.refs.tags.value
-      .split(',')
-      .filter(tag => tag.length)
-      .map(tag => tag.trim().toLowerCase());
-
-    let text = this.refs.text.value;
+    let tags  = this.refs.tags.state.tags;
+    let text  = this.refs.text.value;
 
     let values = {};
 
@@ -24,22 +20,22 @@ const AddMode = React.createClass({
     this.props.saveSnippet(values);
   },
 
-
   render() {
     return(
       <div className="panel-mode">
         <div className="selected-mode">
           <form className="add-snippet-mode" onSubmit={this.createSnippet}>
-            <input id="new-snippet-title" className="" type="text" ref="title" placeholder="Title" />
-            <input id="new-snippet-tags" className="" type="tags" ref="tags" placeholder="Tags" />
-            <textarea id="new-snippet-text" placeholder="put your snipz here" ref="text"></textarea>
-          
+            <input id="new-snippet-title" className="add-field" type="text" ref="title" placeholder="Title" />
+
+            <Tags ref="tags" />
+
+            <textarea id="new-snippet-text" className="add-textarea" placeholder="put your snipz here" ref="text"></textarea>
 
             <PanelControls mode="add" createSnippet={this.createSnippet} />
-          </form>  
+          </form>
         </div>
 
-        
+
       </div>
     );
   }
