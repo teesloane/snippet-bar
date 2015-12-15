a17dd1c43c3b3c91b71af57149afea75cfdbe1fa
Bronek <bronekszulc@outlook.com>
2015-12-15 10:49:49 -0500

    fixed issue with Tags:
    - hitting return from within the title field no longer deletes a tag

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -112,7 +112,7 @@ const Tags = React.createClass({
       return (
         <span className="tag" key={index}>
           <span className="tag-value">{tag}</span>
-          <button className="tag-delete-button" onClick={this.deleteTag.bind(null, tag)}>x</button>
+          <button type="button" className="tag-delete-button" onClick={this.deleteTag.bind(null, tag)}>x</button>
         </span>
       );
     });
