a04dfec703897fc784e92044b8e4861b37c0a493
Tyler Sloane <tylersloane@gmail.com>
2015-12-14 21:09:20 -0500

    Modular buttons success: - created button files - ported fiddle prototype into ^ - Buttons now show up in edit mode, still have to do the other modes. - functions are not hooked up yet, other than a test function in editMode

--- src/components/PanelControls.js
+++ src/components/PanelControls.js
@@ -1,52 +1,31 @@
 const React = require('react');
 
-var PanelControls = React.createClass({
+const Btn = require('./PanelButtons');
+
+
+const PanelControls = React.createClass({
   propTypes: {
     mode:          React.PropTypes.string.isRequired,
     setMode:       React.PropTypes.func,
     createSnippet: React.PropTypes.func
   },
 
+  someFunction() {
+    alert('working');
+  },
+
   showButtons() {
     let mode = this.props.mode;
 
     if (mode === 'edit') {
-      let copy = (
-        <button 
-          key="1"
-          id="copy-snippet"
-          title="Copy Snippet"> 
-          <i className="fa fa-clone fa-2x"></i>
-        </button>
-      );
 
-      let save = (
-        <button 
-          key="2"
-          id="save-snippet"
-          title="Save">
-            <i className="fa fa-floppy-o fa-2x"></i>
-        </button>
-      );
+      let copy = <Btn kind="copy" onClick={this.someFunction}  />
 
-      let del = (
-        <button
-          key="3"
-          id="delete-snippet"  
-          title="delete Snippet">
-          <i className="fa fa-trash fa-2x"></i>
-        </button>
-      );
+      let save = <Btn kind="save" />
 
-      let add = (
-        <button 
-          key="4"
-          id="new-snippet"  
-          title="New Snippet"
-          onClick={this.props.setMode.bind(null, 'add')}>
-              <i className="fa fa-plus fa-2x"></i>
-          </button>
-      );
+      let del = <Btn kind="del" />
+
+      let add = <Btn kind ="add" />
 
       return [
         copy,
