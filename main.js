d6bb5284435d9bf36ff63075183c0162bfb58a51
Bronek <bronekszulc@outlook.com>
2015-12-16 10:35:08 -0500

    refactored main.js to use es2015

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-var ipcMain = require('electron').ipcMain;
-var menubar = require('menubar');
+const ipcMain = require('electron').ipcMain;
+const menubar = require('menubar');
 
-var config = {
+const config = {
   openDevTools: true,
-  title: 'snippets',
-  icon: './app/static/img/brackets.png',
-  iconAlt: './app/static/img/brackets-alt.png'
+  title:        'snippets',
+  icon:         './app/static/img/brackets.png',
+  iconAlt:      './app/static/img/brackets-alt.png'
 };
 
-var mb = menubar({
-  dir: './app',
-  icon: config.icon,
-  width: 600,
-  height: 370,
-  preloadWindow: true,
+const mb = menubar({
+  dir:             './app',
+  icon:            config.icon,
+  width:           600,
+  height:          370,
+  preloadWindow:   true,
   'always-on-top': true
 });
 
@@ -21,13 +21,13 @@ var mb = menubar({
 mb.setOption('resizable', false);
 
 if (config.openDevTools) {
-  mb.on('after-create-window', function() {
+  mb.on('after-create-window', () => {
     mb.window.openDevTools();
   });
 }
 
 // Quit when all windows are closed.
-mb.app.on('window-all-closed', function() {
+mb.app.on('window-all-closed', () => {
   if (process.platform != 'darwin') {
     mb.app.quit();
   }
@@ -35,11 +35,11 @@ mb.app.on('window-all-closed', function() {
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-mb.app.on('ready', function() {
+mb.app.on('ready', () => {
   mb.tray.setToolTip(config.title);
   mb.tray.setPressedImage(config.iconAlt);
 
-  ipcMain.on('mb-app', function(event, arg) {
+  ipcMain.on('mb-app', (event, arg) => {
     if (arg === "quit") {
       console.log('goodbye!');
       mb.app.quit();
