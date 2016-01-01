ed810b38a5a6f13cdd8dabfccedc192872558ad4
broneks <bronek@fastmail.fm>
2016-01-01 12:35:57 -0500

    added a check for duplicate snippets: on update and add, if a snippet already exists with the exact same text, no action is performed

--- src/data.js
+++ src/data.js
@@ -7,13 +7,19 @@ const filePaths = {
 
 const noop = () => {};
 
+let localSnippets = {};
+
 function read(file, callback = noop) {
   if (!file || !filePaths.hasOwnProperty(file)) return;
 
   fs.readFile(filePaths[file], (err, data) => {
     if (err) throw err;
 
-    callback(JSON.parse(data));
+    let jData = JSON.parse(data);
+
+    localSnippets = jData;
+
+    callback(jData);
   });
 }
 
@@ -25,20 +31,26 @@ function write(file, data, callback = noop) {
   fs.writeFile(filePaths[file], jData, (err) => {
     if (err) throw err;
 
+    localSnippets = data;
+
     callback(jData);
   });
 }
 
+function snippetExists(text) {
+  return localSnippets && localSnippets.some(snippet => text === snippet.text);
+}
+
 function settingsModel(syntaxHighlighting) {
   if (typeof syntaxHighlighting === 'undefined') return null;
-  
+
   return {
     syntaxHighlighting
   };
 }
 
-function snippetModel(title = 'untitled', text, tags = [], lang = 'text') {
-  if (!text) return null;
+function snippetModel(title = 'untitled', text, tags = []) {
+  if (!text || snippetExists(text)) return null;
 
   let now = Date.now();
 
