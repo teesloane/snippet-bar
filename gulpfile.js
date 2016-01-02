32f2c80993a0cae5953581da2cfe7628a70caf87
broneks <bronek@fastmail.fm>
2016-01-01 20:31:32 -0500

    improved the gulpfile: - added all paths to an object - added a task for copying over the highlight.js stylesheet - removed gulp-cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
 var gulp    = require('gulp');
 var plumber = require('gulp-plumber');
 var babel   = require('gulp-babel');
-var nano    = require('gulp-cssnano');
 var sass    = require('gulp-sass');
+
+var paths = {
+  app:  'app/',
+  css:  'app/static/css',
+  font: 'app/static/fonts'
+};
+
+var tasks = [
+  'js',
+  'sass',
+  'normalize',
+  'highlightstyles',
+  'fontstyles',
+  'fontfiles'
+];
 
 gulp.task('js', function() {
   gulp.src('src/**/*.js')
     .pipe(plumber())
     .pipe(babel())
-    .pipe(gulp.dest('app/'))
+    .pipe(gulp.dest(paths.app))
 });
 
 gulp.task('sass', function() {
@@ -17,40 +31,32 @@ gulp.task('sass', function() {
     .pipe(sass({
       outputStyle: 'compressed'
     }))
-    .pipe(gulp.dest('./app/static/css/'));
+    .pipe(gulp.dest(paths.css));
 });
 
 gulp.task('normalize', function() {
   gulp.src('node_modules/normalize.css/normalize.css')
-    //.pipe(nano())
-    .pipe(gulp.dest('app/static/css/'));
+    .pipe(gulp.dest(paths.css));
+});
+
+gulp.task('highlightstyles', function() {
+  gulp.src('node_modules/highlight.js/styles/monokai-sublime.css')
+    .pipe(gulp.dest(paths.css));
 });
 
 gulp.task('fontstyles', function() {
   gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-    .pipe(gulp.dest('app/static/css/'));
+    .pipe(gulp.dest(paths.css));
 });
 
 gulp.task('fontfiles', function() {
   gulp.src('node_modules/font-awesome/fonts/*.*')
-    .pipe(gulp.dest('app/static/fonts/'));
+    .pipe(gulp.dest(paths.font));
 });
 
-gulp.task('default', [
-  'js',
-  'sass',
-  'normalize',
-  'fontstyles',
-  'fontfiles'
-], function() {
+gulp.task('default', tasks, function() {
   gulp.watch(['src/**/*.js'], ['js']);
   gulp.watch(['sass/**/*.scss'], ['sass']);
 });
 
-gulp.task('compile', [
-  'js',
-  'sass',
-  'normalize',
-  'fontstyles',
-  'fontfiles'
-]);
+gulp.task('compile', tasks);
